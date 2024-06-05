df56299d707bb32bd686d068849548fe4104ae9b
SCSaw <siew.ching.saw@gmail.com>
2024-06-05 15:00:48 +0800

    Update error from Login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -82,7 +82,7 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
+                // PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
                 KnownAs = user.KnownAs
             };
         }
